67cecd696a16dd084b7c34b703775f23c8038758
ken@27 <82031953+kean7296@users.noreply.github.com>
2022-06-28 18:49:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '363142599223562',
-        clientSecret: 'fb85daa8cb814bdc86da08bc32eb1590'
+        clientId: 'client id',
+        clientSecret: 'client secret id'
     }
 }
